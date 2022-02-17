d23886dce8d1e20c04bb3ed7fa886568a9fee49d
a a <t.mirzeyev@mail.com>
2022-02-17 15:08:32 +0300

    finish

---
+++ lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести квадрат числа
+Console.WriteLine("write a number");
+int num = Convert.ToInt16(Console.ReadLine());
+int kvadrat(int num1){
+    int result=num1*num1;
+    return result;
+    //Console.WriteLine(result);
+
+}
+int sum=kvadrat(num);
+Console.WriteLine(sum);
